34a98ccc4e3c15f9f3805e726e557e003e13a2e2
Revolt77777 <ethanhxw@gmail.com>
2025-07-10 22:40:50 -0700

    Spring boot maven project

---
+++ src/main/java/com/revolt7/leetsync/LeetsyncApplication.java
@@ -0,0 +1,13 @@
+package com.revolt7.leetsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revolt7/leetsync/LeetsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revolt7.leetsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
