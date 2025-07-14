7ac3393d38703cebd6667757427732bfe2a3a3bc
Revolt77777 <ethanhxw@gmail.com>
2025-07-13 18:53:32 -0700

    Restructure into 3 sub-apps and create rest-api-server app

---
+++ rest-api-server/src/main/java/com/leetsync/api/rest_api_server/RestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.leetsync.api.rest_api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiServerApplication.class, args);
+	}
+
+}
---
+++ rest-api-server/src/test/java/com/leetsync/api/rest_api_server/RestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetsync.api.rest_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
